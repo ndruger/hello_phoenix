368a2692b559f1d91fefb7e3b901d985bc215c03
ndruger <snowfullnight@gmail.com>
2015-07-22 13:06:22 +0900

    material ui

--- web/static/js/components/dashboard.js
+++ web/static/js/components/dashboard.js
@@ -1,8 +1,33 @@
 import React from "react";
 import {Route, Link} from 'react-router';
+import mui from "material-ui";
 
 import BookCollection from "../collections/book_collection"
 import BackboneMixin from "backbone-react-component";
+
+let ThemeManager = new mui.Styles.ThemeManager();
+let RaisedButton = mui.RaisedButton;
+
+let SomeAwesomeComponent = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext: function() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+
+  render: function() {
+    return (
+        <RaisedButton label="Default" />
+    );
+  }
+
+});
+
 
 let Dashboard = React.createClass({
   // mixins: [backboneMixin, Router.State],
@@ -11,6 +36,7 @@ let Dashboard = React.createClass({
     // console.log('render', this.state, this.props)
     return (
       <div>
+        <SomeAwesomeComponent/>
         {
           this.getCollection().map((item) => {
             return <div>{item.get('id')}</div>;
