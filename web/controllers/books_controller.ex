7b48d973415885141addf249b0f6584a68359d32
ndruger <snowfullnight@gmail.com>
2015-09-24 00:54:03 +0900

    log_message_after_action

--- web/controllers/books_controller.ex
+++ web/controllers/books_controller.ex
@@ -3,8 +3,7 @@ defmodule HelloPhoenix.BooksController do
   require Logger
 
   plug :log_message, "before action"
-  plug :action
-  plug :log_message, "after action"
+  plug :log_message_after_action, "after action"
 
   def index(conn, _params) do
     Logger.debug("index action")
@@ -14,6 +13,14 @@ defmodule HelloPhoenix.BooksController do
   defp log_message(conn, msg) do
     Logger.debug("log_message: " <> msg)
     conn
+  end
+
+  defp log_message_after_action(conn, msg) do
+    conn
+    |> register_before_send fn (conn) ->
+      conn
+      |> log_message(msg)
+    end
   end
 
 end
