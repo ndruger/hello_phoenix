c6d289c411fabafcc97d1c58afae8349348e5a64
ndruger <snowfullnight@gmail.com>
2015-09-23 22:31:51 +0900

    test of "after action"

--- web/controllers/books_controller.ex
+++ web/controllers/books_controller.ex
@@ -2,8 +2,18 @@ defmodule HelloPhoenix.BooksController do
   use HelloPhoenix.Web, :controller
   require Logger
 
+  plug :log_message, "before action"
+  plug :action
+  plug :log_message, "after action"
+
   def index(conn, _params) do
+    Logger.debug("index action")
     json conn, [%{id: "neko"}, %{id: "neko2"}]
+  end
+
+  defp log_message(conn, msg) do
+    Logger.debug("log_message: " <> msg)
+    conn
   end
 
 end
