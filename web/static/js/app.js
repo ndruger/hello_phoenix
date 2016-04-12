8e6a5df081dc5c0423e5469deac7c992baac372f
ndruger <snowfullnight@gmail.com>
2016-04-13 07:48:07 +0900

    webpack

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next-client';
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 
 function counter(state = 0, action) {
   switch (action.type) {
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       'chai',
       'react/addons',
       'material-ui',
-    ]
+    ],
   },
   output: {
     path: __dirname + '/priv/static/assets',
@@ -31,11 +31,11 @@ module.exports = {
   module: {
     loaders: [
       // { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-      {test: /\.js$/, loader: "babel", query: {presets: ["react", "es2015", "stage-0"]}},
-      { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.css$/, loader: 'style!css!sass' },
-      { test: /\.png$/, loader: "file-loader" },
-      { test: /\.jpg$/, loader: "file-loader" },
+      {test: /\.js$/, loader: 'babel'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.css$/, loader: 'style!css!sass'},
+      {test: /\.png$/, loader: 'file-loader'},
+      {test: /\.jpg$/, loader: 'file-loader'},
     ],
     postLoaders: (function() {
       if (process.env.NODE_ENV !== 'test') {
@@ -44,15 +44,21 @@ module.exports = {
       return [{
         test: /\.js$/,
         exclude: /(node_modules\/|\.spec|\.webpack\.js)/,
-        loader: 'istanbul-instrumenter'
+        loader: 'istanbul-instrumenter',
       }];
-    })()
+    })(),
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin(
-      "vendor",
-      "vendor.js"
-    )
-  ],
+  plugins: (function() {
+    const plugins = [];
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new webpack.optimize.CommonsChunkPlugin(
+      'vendor',
+      'vendor.js'
+    ));
+    if (process.env.NODE_ENV !== undefined || process.env.NODE_ENV !== 'development') {
+      plugins.push(new webpack.optimize.UglifyJsPlugin());
+    }
+    return plugins;
+  })(),
 };
 
